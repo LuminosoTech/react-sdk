46f6e647e2a9805ac0ee077d9e40273862899ff9
Fabrizio Rodin-Miron <fabrizio.rodin-miron@adsdata.ca>
2021-07-18 23:44:23 -0400

    update(deps): updated feature flag sdk package & bumped version

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -5,8 +5,6 @@ import { LuminosoProvider } from "../store";
 interface InitializeProps {}
 
 export const initialize = (sdkKey: string, user: User) => (props: React.PropsWithChildren<InitializeProps>) => {
-  console.log("sdkKey", sdkKey);
-
   const instance = createInstance({ sdkKey }, user);
 
   return <LuminosoProvider instance={instance}>{props.children}</LuminosoProvider>;
